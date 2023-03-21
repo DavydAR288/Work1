94f1e20c05eefea4bdcfa25ce19848b20d5f6314
Davyd.AR <9766288@mail.ru>
2023-03-21 20:03:36 +0300

    Create base classes

---
+++ Classes/ClassList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassList
+{
+    public class ArrayMaster
+    {
+        public static void CreateFromKeyboard()
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+        public static void PrintArray(string[] array) // вывод матрицы в консоль
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write($"{array[i],6} "); // форматирование строки
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+    public class ConsoleMaster
+    {
+        public static string PromtString(string message) // ввод с консоли
+        {
+            Console.Write(message);
+            string? value = Console.ReadLine();
+            return value;
+        }
+        public static int PromtInt(string message) // ввод с консоли
+        {
+            Console.Write(message);
+            string value = (Console.ReadLine());
+            int result = Convert.ToInt32(value);
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassList;
+
+Console.Clear();
+ArrayMaster.CreateFromKeyboard();
+string s = ConsoleMaster.PromtString("Input m ");
+Console.WriteLine(s);
