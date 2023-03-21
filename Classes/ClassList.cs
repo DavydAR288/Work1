3c7edae649067526debe68d9a64eb45295f694bc
Davyd.AR <9766288@mail.ru>
2023-03-21 21:42:52 +0300

    Correct methods CreateFromKeyboard, PrintArray, CreateNewArray

--- Classes/ClassList.cs
+++ Classes/ClassList.cs
@@ -8,36 +8,59 @@ namespace ClassList
 {
     public class ArrayMaster
     {
-        public static void CreateFromKeyboard()
+        /// <summary>
+        /// Создание строчного массива
+        /// </summary>
+        /// <param name="elements">получаемая длина массива</param>
+        /// <returns></returns>
+        public static string[] CreateFromKeyboard(int length)
         {
-            Console.WriteLine("Hello, World!");
-
+            Console.WriteLine($"Создание строчного массива с кличеством элементов {length}");
+            string[] matrix = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"Введите слово №{i+1}: ");
+                string? inputWord=Console.ReadLine();
+                if (inputWord != "") matrix[i] = inputWord;
+                else i--;
+            }
+            return matrix;
         }
-        public static void PrintArray(string[] array) // вывод матрицы в консоль
+        public static string[] CreateNewArray(string[] array, int length)
         {
+            int count = 0;
+            int countNew = 0;
             for (int i = 0; i < array.GetLength(0); i++)
             {
-                Console.Write($"{array[i],6} "); // форматирование строки
-                Console.WriteLine();
+                if (array[i].Length <= 3) count++;
             }
+            
+            string[] arrayNew = new string[count];
+            for (int i = 0; i < length; i++)
+            {
+                if (array[i].Length <= 3)
+                {
+                    arrayNew[countNew] = array[i];
+                    countNew++;
+                }
+            }
+            return arrayNew;
         }
-    }
 
-
-    public class ConsoleMaster
-    {
-        public static string PromtString(string message) // ввод с консоли
+        /// <summary>
+        /// Вывод в консоль строчного массива
+        /// </summary>
+        /// <param name="array">Строчный массив для вывода</param>
+        public static void PrintArray(string[] array, string textOutput) 
         {
-            Console.Write(message);
-            string? value = Console.ReadLine();
-            return value;
-        }
-        public static int PromtInt(string message) // ввод с консоли
-        {
-            Console.Write(message);
-            string value = (Console.ReadLine());
-            int result = Convert.ToInt32(value);
-            return result;
+            Console.Write($"{textOutput}");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write($"{array[i]}");
+                if (i != array.GetLength(0)-1) Console.Write($", ");
+                else Console.Write($".");
+            }
+            Console.WriteLine();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿using ClassList;
 
 Console.Clear();
-ArrayMaster.CreateFromKeyboard();
-string s = ConsoleMaster.PromtString("Input m ");
-Console.WriteLine(s);
+int lengthArray = 3;
+string[] ArrayOriginal = ArrayMaster.CreateFromKeyboard(lengthArray);
+ArrayMaster.PrintArray(ArrayOriginal, "Строчный массив: ");
+Console.WriteLine();
+int lengthString = 3;
+string[] ArrayNew = ArrayMaster.CreateNewArray(ArrayOriginal, lengthString);
+ArrayMaster.PrintArray(ArrayNew, $"Новый массив со словами ограниченными длинной в {lengthString} символа: ");
